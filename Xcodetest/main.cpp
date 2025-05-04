efe62ac50047367b2c7d68b2639ac451b987a381
Lasitha Atapattu <lasithaatapattu18@gmail.com>
2025-05-04 12:13:36 +0530

    Initial Commit

---
+++ Xcodetest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Xcodetest
+//
+//  Created by Lasitha Atapattu on 2025-05-04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
